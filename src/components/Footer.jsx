035b4aa88ddcd304b2605a882d05ef8dfffc054f
ankitkumarchoudhary01 <choudharyankit0852@gmail.com>
2026-07-16 03:47:02 +0530

    modified Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,11 @@
 export default function Footer() {
   return (
-<<<<<<< HEAD
-    <footer className="footer">
-      <div>
-        Copyright © 2026 Prodesk IT.
-      </div>
-      <div>
-        All Rights Reserved.
-      </div>
-
-      <div className="socials">
-        <span><a href="#"><img src="\insta.png" alt="instagramlogo" /></a></span>
-        <span><a href="#"><img src="\twitter.png" alt="twitterlogo" /></a></span>
-=======
     <footer className="flex flex-col md:flex-row items-center justify-between px-6 md:px-16 py-4 bg-[var(--primary)] text-white border-t border-white">
 
       <div className="w-60">
         <p className="my-2 text-center">
           Copyright © 2026 Prodesk IT
         </p>
->>>>>>> phase-2-tailwind
       </div>
 
       <div className="w-60">
